7852f1feec0b9e656a1ebd72010d1fd3720af2ff
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-01 09:41:39 -0500

    change all responses

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { createUser } = require('./src/users')
+const { createUser, loginUser } = require('./src/users')
 const PORT = process.env.PORT || 3001
 
 const app = express()
@@ -8,6 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 app.post('/users', createUser)
+app.post('/users/login', loginUser)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
--- src/users.js
+++ src/users.js
@@ -4,11 +4,14 @@ exports.createUser = (req, res) => {
   // first, let's do some validation (email, password)
   if (!req.body || !req.body.email || !req.body.password) {
     // invalid request
-    res.status(400).send("Invalid request");
+    res.status(400).send({
+      success: false,
+      message: "Invalid request",
+    });
     return;
   }
   const newUser = {
-    email: req.body.email,
+    email: req.body.email.toLowerCase(),
     password: req.body.password,
     isAdmin: false,
     userRole: 5,
@@ -17,8 +20,63 @@ exports.createUser = (req, res) => {
   db.collection("users")
     .add(newUser)
     .then((doc) => {
+      const user = { // this will become the payload for our JWT
+        id: doc.id,
+        email: newUser.email,
+        isAdmin: false,
+        userRole: 5,
+      }
       // TODO: create a JWT and send back the token
-      res.status(201).send("Account created");
+      res.status(201).send({
+        success: true,
+        message: "Account created",
+        token: user, // add this to token later
+
+      });
     })
-    .catch((err) => res.status(500).send(err));
+    .catch((err) => res.status(500).send({
+      success: false,
+      message: err.message,
+      error: err
+    }));
+};
+
+exports.loginUser = (req, res) => {
+  // first, let's do some validation (email, password)
+  if (!req.body || !req.body.email || !req.body.password) {
+    // invalid request
+    res.status(400).send("Invalid request");
+    return;
+  }
+  const db = connectDb()
+  db.collection('users')
+  .where('email','==', req.body.email.toLowerCase())
+  .where('password','==', req.body.password)
+  .get()
+    .then(snapshot => {
+      if(snapshot.empty) {
+        res.status(401).send({
+          success: false,
+          message: 'Invalid email or password',
+        })
+        return // makes sure it doesn't continue processing
+      }
+      // good login part here
+      const users = snapshot.docs.map(doc => {
+      let user = doc.data()
+      user.id = doc.id
+      user.password = undefined
+      return user
+      })
+      res.send({
+        success: true,
+        message: 'Login successful',
+        token: users[0]
+      })
+    })
+    .catch((err) => res.status(500).send({
+      success: false,
+      message: err.message,
+      error: err
+    }));
 };
