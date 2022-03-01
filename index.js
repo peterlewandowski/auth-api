f05adef2ee7526f582874e80c2001b00b70deab2
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-01 09:57:48 -0500

    added users route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { createUser, loginUser } = require('./src/users')
+const { createUser, loginUser, getUSers } = require('./src/users')
 const PORT = process.env.PORT || 3001
 
 const app = express()
@@ -9,6 +9,7 @@ app.use(cors())
 
 app.post('/users', createUser)
 app.post('/users/login', loginUser)
+app.get('/users', getUSers)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
--- src/users.js
+++ src/users.js
@@ -20,25 +20,27 @@ exports.createUser = (req, res) => {
   db.collection("users")
     .add(newUser)
     .then((doc) => {
-      const user = { // this will become the payload for our JWT
+      const user = {
+        // this will become the payload for our JWT
         id: doc.id,
         email: newUser.email,
         isAdmin: false,
         userRole: 5,
-      }
+      };
       // TODO: create a JWT and send back the token
       res.status(201).send({
         success: true,
         message: "Account created",
         token: user, // add this to token later
-
       });
     })
-    .catch((err) => res.status(500).send({
-      success: false,
-      message: err.message,
-      error: err
-    }));
+    .catch((err) =>
+      res.status(500).send({
+        success: false,
+        message: err.message,
+        error: err,
+      })
+    );
 };
 
 exports.loginUser = (req, res) => {
@@ -48,35 +50,65 @@ exports.loginUser = (req, res) => {
     res.status(400).send("Invalid request");
     return;
   }
-  const db = connectDb()
-  db.collection('users')
-  .where('email','==', req.body.email.toLowerCase())
-  .where('password','==', req.body.password)
-  .get()
-    .then(snapshot => {
-      if(snapshot.empty) {
+  const db = connectDb();
+  db.collection("users")
+    .where("email", "==", req.body.email.toLowerCase())
+    .where("password", "==", req.body.password)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
         res.status(401).send({
           success: false,
-          message: 'Invalid email or password',
-        })
-        return // makes sure it doesn't continue processing
+          message: "Invalid email or password",
+        });
+        return; // makes sure it doesn't continue processing
       }
       // good login part here
-      const users = snapshot.docs.map(doc => {
-      let user = doc.data()
-      user.id = doc.id
-      user.password = undefined
-      return user
-      })
+      const users = snapshot.docs.map((doc) => {
+        let user = doc.data();
+        user.id = doc.id;
+        user.password = undefined;
+        return user;
+      });
       res.send({
         success: true,
-        message: 'Login successful',
-        token: users[0]
-      })
+        message: "Login successful",
+        token: users[0],
+      });
     })
-    .catch((err) => res.status(500).send({
-      success: false,
-      message: err.message,
-      error: err
-    }));
+    .catch((err) =>
+      res.status(500).send({
+        success: false,
+        message: err.message,
+        error: err,
+      })
+    );
+};
+
+exports.getUSers = (req, res) => {
+  // TODO: protect this route with JWT
+  const db = connectDb();
+  db.collection("users")
+    .get()
+    .then((snapshot) => {
+      const users = snapshot.docs.map((doc) => {
+        let user = doc.data();
+        user.id = doc.id;
+        user.password = undefined;
+        return user;
+      });
+      res
+        .send({
+          success: true,
+          message: "Users returned",
+          users
+        })
+        .catch((err) =>
+          res.status(500).send({
+            success: false,
+            message: err.message,
+            error: err,
+          })
+        );
+    });
 };
